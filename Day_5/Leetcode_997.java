4a114ac662da05c295fec9894317777323cdb89e
Krishan Mohan <krishanmohan3501@gmail.com>
2024-07-24 22:55:18 +0530

    updates

---
+++ Day_5/Leetcode_997.java
@@ -0,0 +1,33 @@
+package Day_5;
+
+
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int n = nums.length;
+        int[] result = new int[n];
+        int left = 0, right = n - 1;
+        int index = n - 1;
+
+        while (left <= right) {
+            int leftSquare = nums[left] * nums[left];
+            int rightSquare = nums[right] * nums[right];
+
+            if (leftSquare > rightSquare) {
+                result[index] = leftSquare;
+                left++;
+            } else {
+                result[index] = rightSquare;
+                right--;
+            }
+            index--;
+        }
+
+        return result;
+    }
+}
+
+public class Leetcode_997 {
+    public static void main(String[] args) {
+
+    }
+}
