7eb6cc69232eb9a0ba2bbf48ecbc827b32945ce0
Krishan Mohan <krishanmohan3501@gmail.com>
2024-07-31 23:03:58 +0530

    updates

---
+++ Day_12/Leetcode_2089.java
@@ -0,0 +1,19 @@
+package Day_12;
+import java.util.*;
+
+class Solution {
+    public List<Integer> targetIndices(int[] nums, int target) {
+
+        Arrays.sort(nums);
+        List <Integer> result = new ArrayList<>();
+        for(int i = 0; i < nums.length;i++){
+            if(nums[i]==target){
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
+
+public class Leetcode_2089 {
+}
