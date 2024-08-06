57e9d1aebf07949ef1d5095fad87b39331a8b8a2
Krishan Mohan <krishanmohan3501@gmail.com>
2024-08-06 22:05:27 +0530

    updates

---
+++ Day_15/Leetcode_151.java
@@ -0,0 +1,20 @@
+package Day_15;
+
+class Solution {
+    public String reverseWords(String s) {
+        String [] words =s.trim().split("\s+");
+
+        StringBuilder ans = new StringBuilder();
+        for(int i = words.length-1; i >= 0; i--){
+            ans.append(words[i]);
+            if(i > 0){
+                ans.append(" ");
+            }
+        }
+        return ans.toString();
+    }
+}
+
+public class Leetcode_151 {
+
+}
